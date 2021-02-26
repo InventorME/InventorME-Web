e66ff20a6e98c3163b82994ff13fc8a785ef172f
jtm029 <43485929+jtm029@users.noreply.github.com>
2021-02-25 20:23:56 -0600

    Added about to nav

--- src/aboutPage/AboutPage.js
+++ src/aboutPage/AboutPage.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import './AboutPage.css';
 import InventorLogo from '../images/InventorMeLogo.png';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import BackButton from '../images/back-button.png'
 class AboutPage extends Component{
     
     render(){
     return (
         <div class="home-title">
-        <div class="home-inventor-title"></div>
+        <Link to="/accounts-page" style={{ textDecoration: 'none' }}>
+          <img src={BackButton} class="profile-back" alt="back" />
+        </Link> 
         <img src={InventorLogo} class="center" alt="" />
         <p className = 'primary'>InventorMe is a new, revolutionary management app that prioritizes and streamlines efficiency for
         digital photos, documents, and other items that you need to oversee.
         You now have the power to manage all of your belongings with just the tip of your fingers.  </p>
-        
         </div>
-    
       );
     }
 }
