aa470e6d14675a7113ca329f73191320e4539135
Estebanadm <70405222+Estebanadm@users.noreply.github.com>
2021-04-05 16:27:53 -0500

    Collections update

--- src/navigationBanner/collections/Collections.js
+++ src/navigationBanner/collections/Collections.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import './Collections.css';
-import NavBanner from '../NavBanner.js'
+import NavBanner from '../NavBanner.js';
+import { AiOutlinePlusCircle } from "react-icons/ai";
+
 
 class Collections extends Component {
+  renderFolders = (props) =>{
+    return(
+      <div className='area' style={{backgroundColor:props.color}}>
+            <AiOutlinePlusCircle onClick={() => alert('Clicked')} class={'icon'}/>
+            <div className='textTittle'>{props.tittle}</div>
+            <div className='textAmount'>Items: {props.amount}</div>
+      </div>
 
+    );
+  }
   render() {
       return (
       <div>
           <NavBanner/>
-          <div className='area' style={{backgroundColor:'#0e7e92'}}>
-            
-              
-          </div>
+          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
+          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
+          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
+          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
+          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
+          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
+          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
       </div>
       );
     }
