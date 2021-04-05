53495111760f5271fe8c8bad9970c25c1c1b493a
jtm029 <43485929+jtm029@users.noreply.github.com>
2021-04-05 14:34:50 -0500

    reverted dark theme

--- src/util/objectColors.js
+++ src/util/objectColors.js
@@ -2,17 +2,17 @@ function colorGenerator(){
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
         const color = {
             object: ['#ffb5b9', '#b3b5ff', '#47ff72', '#aebffc', '#b3b5ff','#ffb5b9', '#b3b5ff', '#47ff72'],
-            fill: '#979797',
+            fill: '#404040',
             background: '#333333',
-            label: '#000000'
+            label: '#ffffff'
         }
         return color;
     }
     else{
         const color = {
-            object: ['#ffb5b9', '#b3b5ff', '#47ff72', '#aebffc', '#b3b5ff', '#ffb5b9', '#b3b5ff', '#47ff72'],
-            fill: '#f2f2f2',
-            background: '#ffffff',
+            object: ['#ffb5b9', '#b3b5ff', '#47ff72', '#aebffc', '#b3b5ff','#ffb5b9', '#b3b5ff', '#47ff72'],
+            fill: '#979797',
+            background: '#333333',
             label: '#000000'
         }
         return color;
