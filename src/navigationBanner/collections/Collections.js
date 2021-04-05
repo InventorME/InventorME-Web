398bf4c93c0c6cd406a48f5409d850527597caff
Estebanadm <70405222+Estebanadm@users.noreply.github.com>
2021-04-05 17:32:38 -0500

    Collection folders working with backend

--- src/navigationBanner/collections/Collections.js
+++ src/navigationBanner/collections/Collections.js
@@ -2,9 +2,51 @@ import React, { Component } from 'react';
 import './Collections.css';
 import NavBanner from '../NavBanner.js';
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import { Database } from '../../util/Database';
 
 
 class Collections extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        items: [], editItem: false, item: null
+    }
+  }
+  componentDidMount() {
+    this.getItems();
+  }
+  filterItemByID(ID, collectionName) {
+        this.setState({ item: this.state.items[collectionName].filter(item => item.itemID === ID), editItem: true });
+    }
+  getItems = async () => {
+    const db = new Database();
+    try {
+        const body = await db.get();
+        
+        let collectionItems = [];
+        if (body.items.length > 0)
+        collectionItems = body.items.filter(item => item.Collections !== null)
+
+        var groupBy = function (xs, key) {
+            return xs.reduce(function (rv, x) {
+                (rv[x[key]] = rv[x[key]] || []).push(x);
+                return rv;
+            }, {});
+        };
+
+        var groubedByTeam = groupBy(collectionItems , 'itemCategory')
+        this.setState({ items: groubedByTeam })
+        this.setState({ collectionTittle: Object.keys(groubedByTeam) })
+        console.log("ITEMS");
+        console.log(groubedByTeam);
+        
+        this.render();
+        this.setState({ loading: false });
+    }
+    catch (error) {
+        console.log('Error pulling data', error);
+    }
+}
   renderFolders = (props) =>{
     return(
       <div className='area' style={{backgroundColor:props.color}}>
@@ -19,13 +61,13 @@ class Collections extends Component {
       return (
       <div>
           <NavBanner/>
-          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
-          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
-          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
-          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
-          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
-          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
-          <this.renderFolders tittle="Tittle number 1" amount='50' color='red'/>
+          {this.state.collectionTittle ? this.state.collectionTittle.map((collectionTittle) => (
+            <div className='area' style={{backgroundColor:'red'}}>
+            <AiOutlinePlusCircle onClick={() => alert('Clicked')} class={'icon'}/>
+            <div className='textTittle'>{collectionTittle}</div>
+            <div className='textAmount'>Items: {'5'}</div>
+      </div>
+          )):null}
       </div>
       );
     }
