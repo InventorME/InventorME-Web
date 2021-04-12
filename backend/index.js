67dc78658d6b4a34f0ad907288874aa82363dfc7
jtm029 <43485929+jtm029@users.noreply.github.com>
2021-04-11 19:55:13 -0500

    Added dev command

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
 const requestRepository = require('./repository/requestRepository');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(pino);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
 app.get('/api/getBarcodeItem', (req, res) => {
   requestRepository.getItemByBarcode(req.query.code).then(result => { res.json(result)}); 
 });
