029a2d1bd84f8439c79b0f6be7150a5e9ceff0a6
jtm029 <43485929+jtm029@users.noreply.github.com>
2021-04-15 13:31:06 -0500

    barcode search

--- backend/index.js
+++ backend/index.js
@@ -15,14 +15,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(__dirname)); 
 
-app.get('/api/getBarcodeItem', (req, res) => {
-  getItemByBarcode(req.query.code).then(response => res.json(response))
-});
-
-// app.get('/*', function(request, response) {
-//   response.sendFile(path.join(__dirname, '../src', 'index.html'));
-// });
-
+if(process.env.NODE_ENV === 'production') {
+  app.get('/api/getBarcodeItem', (req, res) => {
+    getItemByBarcode(req.query.code).then(response => res.json(response))
+  });
+}
 async function getItemByBarcode(barcode) {
   let browserInstance = browserObject.startBrowser();
   let response = await scraperController(browserInstance, barcode)
